47f4755ec68ef31fe806b0c874bf1f4436723e15
Sean Zimbelman <zimbiekitten@gmail.com>
2020-09-11 18:32:00 -0700

    added decimal to binary

--- math.js
+++ math.js
@@ -1,6 +1,16 @@
-let binary = prompt("Test?");
+let decimal = prompt("Test?");
 
-function bin_to_hexadec(number) { 
-    return parseInt((number) .replace(/[^01]/), 2).toString(16); };
+function dec_to_bh(number1, base) {
+    while (number1 < 0) {
+        number1 = 0xFFFFFFFF + number1 + 1;
+    };
+    switch (base) {
+        case 'B':
+            return parseInt(number1, 10).toString(2);
+        case 'H':
+            return parseInt(number1, 10).toString(16)
+    }
+};
 
-console.log(bin_to_hexadec(binary));
+console.log(dec_to_bh(decimal,'H'))
+console.log(dec_to_bh(decimal,'B'))
