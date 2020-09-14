002e927623fb33dc9c4c737fb37d27c2cff58293
Sean Zimbelman <Zimbiekitten@gmail.com>
2020-09-14 16:20:27 -0700

    Fixed sentex

--- math.js
+++ math.js
@@ -5,9 +5,9 @@ function dec_to_bh(number1, type) {
         case 'bin':
             return parseInt(number1, 10).toString(2);
         case 'hex':
-            return parseInt(number1, 10).toString(16)
+            return parseInt(number1, 10).toString(16);
     }
 };
 
-console.log(`The number ${decimal} in hexadecimal is ${dec_to_bh(decimal,'hex')}`)
-console.log(`The number ${decimal} in binary is ${dec_to_bh(decimal,'bin')}`)
+console.log(`The number ${decimal} in hexadecimal is ${dec_to_bh(decimal,'hex')}`);
+console.log(`The number ${decimal} in binary is ${dec_to_bh(decimal,'bin')}`);
